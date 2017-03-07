f0bd5a5cfd185a071ac454454919ba9448a4ae9c
Marvin Aleman <mraleman@gmail.com>
2017-03-07 09:55:16 -0500

    fixed a session issue

--- app/init.php
+++ app/init.php
@@ -10,110 +10,117 @@ require_once __DIR__.'/../vendor/autoload.php';
  * may want to store this in the in a file on the server and update the data if
  * needed periodically simulating an Application State with PHP.
  */
-$_SESSION['metadata'] = [
-    1 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Full-Time'
-    ],
-    2 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Part-Time'
-    ],
-    3 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Seasonal'
-    ],
-    4 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Temp'
-    ],
-    5 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Itern'
-    ],
-    6 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Volunteer'
-    ],
-    7 => [
-        'metadata_type' => 'experience',
-        'metadata_label' => 'Entry'
-    ],
-    8 => [
-        'metadata_type' => 'experience',
-        'metadata_label' => 'Junior'
-    ],
-    9 => [
-        'metadata_type' => 'experience',
-        'metadata_label' => 'Mid-Senior'
-    ],
-    10 => [
-        'metadata_type' => 'experience',
-        'metadata_label' => 'Senior'
-    ],
-    11 => [
-        'metadata_type' => 'comptype',
-        'metadata_label' => 'Annum'
-    ],
-    12 => [
-        'metadata_type' => 'comptype',
-        'metadata_label' => 'Hour'
-    ],
-    13 => [
-        'metadata_type' => 'jobtype',
-        'metadata_label' => 'Contract'
-    ]
-];
+if (!isset($_SESSION['metadata'])) {
+    $_SESSION['metadata'] = [
+        1 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Full-Time'
+        ],
+        2 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Part-Time'
+        ],
+        3 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Seasonal'
+        ],
+        4 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Temp'
+        ],
+        5 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Itern'
+        ],
+        6 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Volunteer'
+        ],
+        7 => [
+            'metadata_type' => 'experience',
+            'metadata_label' => 'Entry'
+        ],
+        8 => [
+            'metadata_type' => 'experience',
+            'metadata_label' => 'Junior'
+        ],
+        9 => [
+            'metadata_type' => 'experience',
+            'metadata_label' => 'Mid-Senior'
+        ],
+        10 => [
+            'metadata_type' => 'experience',
+            'metadata_label' => 'Senior'
+        ],
+        11 => [
+            'metadata_type' => 'comptype',
+            'metadata_label' => 'Annum'
+        ],
+        12 => [
+            'metadata_type' => 'comptype',
+            'metadata_label' => 'Hour'
+        ],
+        13 => [
+            'metadata_type' => 'jobtype',
+            'metadata_label' => 'Contract'
+        ]
+    ];
+}
+if (!isset($_SESSION['currency'])) {
+    $_SESSION['currency'] = [
+        1 => [
+            'currency_name' => 'US Dollar',
+            'currency_code' => 'USD',
+            'currency_symbol' => '$',
+            'currency_cent' => '¢'
+        ],
+        2 => [
+            'currency_name' => 'British Pound',
+            'currency_code' => 'GBP',
+            'currency_symbol' => '£',
+            'currency_cent' => 'p'
+        ],
+        3 => [
+            'currency_name' => 'Canadian Dollar',
+            'currency_code' => 'CAD',
+            'currency_symbol' => '$',
+            'currency_cent' => '¢'
+        ],
+        4 => [
+            'currency_name' => 'Euro',
+            'currency_code' => 'EUR',
+            'currency_symbol' => '€',
+            'currency_cent' => 'c'
+        ],
+        5 => [
+            'currency_name' => 'Japanese Yen',
+            'currency_code' => 'JPY',
+            'currency_symbol' => '¥',
+            'currency_cent' => 's'
+        ],
+    ];
+}
+if (!isset($_SESSION['country'])) {
+    $_SESSION['country'] = [
+        201 => [
+            'country_abbr' => 'US',
+            'country_name' => 'United States'
+        ]
+    ];
+}
+if (!isset($_SESSION['country_code'])) {
+    $_SESSION['country_code'] = [
+        225 => [
+            'code' => '+1',
+            'country_name' => 'United States'
+        ]
+    ];
 
-$_SESSION['currency'] = [
-    1 => [
-        'currency_name' => 'US Dollar',
-        'currency_code' => 'USD',
-        'currency_symbol' => '$',
-        'currency_cent' => '¢'
-    ],
-    2 => [
-        'currency_name' => 'British Pound',
-        'currency_code' => 'GBP',
-        'currency_symbol' => '£',
-        'currency_cent' => 'p'
-    ],
-    3 => [
-        'currency_name' => 'Canadian Dollar',
-        'currency_code' => 'CAD',
-        'currency_symbol' => '$',
-        'currency_cent' => '¢'
-    ],
-    4 => [
-        'currency_name' => 'Euro',
-        'currency_code' => 'EUR',
-        'currency_symbol' => '€',
-        'currency_cent' => 'c'
-    ],
-    5 => [
-        'currency_name' => 'Japanese Yen',
-        'currency_code' => 'JPY',
-        'currency_symbol' => '¥',
-        'currency_cent' => 's'
-    ],
-];
-
-$_SESSION['country'] = [
-    201 => [
-        'country_abbr' => 'US',
-        'country_name' => 'United States'
-    ]
-];
-
-$_SESSION['country_code'] = [
-    225 => [
-        'code' => '+1',
-        'country_name' => 'United States'
-    ]
-];
-
-$_SESSION['industry'] = [
-    10 => [
-        'industry_name' => 'Transit and Ground Passenger Transportation'
-    ]
-];
+}
+if (!isset($_SESSION['industry'])) {
+    $_SESSION['industry'] = [
+        10 => [
+            'industry_name' => 'Transit and Ground Passenger Transportation'
+        ]
+    ];
+}
